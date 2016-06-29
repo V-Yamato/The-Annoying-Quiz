9712cdf7b5ed5fafd26270a65f963384845c186e
黄聪 <huangcong@huangcongdeMacBook-Pro.local>
2016-06-29 18:50:18 +0800

    初始化项目

---
+++ The Annoying QuizTests/The_Annoying_QuizTests.m
@@ -0,0 +1,39 @@
+//
+//  The_Annoying_QuizTests.m
+//  The Annoying QuizTests
+//
+//  Created by 黄聪 on 16/6/29.
+//  Copyright © 2016年 黄聪. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface The_Annoying_QuizTests : XCTestCase
+
+@end
+
+@implementation The_Annoying_QuizTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
