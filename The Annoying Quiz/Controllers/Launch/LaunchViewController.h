584dddfa4c83410c2611848ac901742f5c6c347c
黄聪 <huangcong@huangcongdeMacBook-Pro.local>
2016-07-01 17:36:05 +0800

    添加masonry，完成启动界面1，启动界面2，开始界面的初步雏形。

--- The Annoying Quiz/Controllers/Launch/LaunchViewController.h
+++ The Annoying Quiz/Controllers/Launch/LaunchViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  LaunchViewController.h
 //  The Annoying Quiz
 //
-//  Created by 黄聪 on 16/6/29.
+//  Created by 黄聪 on 16/6/30.
 //  Copyright © 2016年 黄聪. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LaunchViewController : UIViewController
 
 @end
-
---
+++ The Annoying Quiz/Controllers/Start/StartViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartViewController.h
+//  The Annoying Quiz
+//
+//  Created by 黄聪 on 16/7/1.
+//  Copyright © 2016年 黄聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartViewController : UIViewController
+
+@end
