1df237c0b1808be388529a07c1d4255874b33ac9
黄聪 <huangcong@huangcongdeMacBook-Pro.local>
2016-07-05 11:01:39 +0800

    将app默认横屏，简单美化开始界面，取消右划手势。

--- The Annoying Quiz/Controllers/Launch/LaunchViewController.m
+++ The Annoying Quiz/Controllers/Launch/LaunchViewController.m
@@ -18,7 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.navigationController.navigationBar setHidden:YES];
+    [self.navigationController.interactivePopGestureRecognizer setEnabled:NO];
     [self setUpSubViews];
 }
 
